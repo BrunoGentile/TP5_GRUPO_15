795aaa2e5788d653f49797b04cfa28f4926f9228
jonathan <jonathan@DESKTOP-RQAQDE9>
2025-05-02 20:53:30 -0300

    cree la clase conexion y la declare un objeto en el eje2

--- TP5_GRUPO_15/ListarSucursal.aspx.cs
+++ TP5_GRUPO_15/ListarSucursal.aspx.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Data.SqlClient;
 using System.Web;
 using System.Web.UI;
 using System.Web.UI.WebControls;
@@ -9,6 +10,7 @@ namespace TP5_GRUPO_15
 {
     public partial class Ejercicio2 : System.Web.UI.Page
     {
+        private conexion conexion = new conexion();
         protected void Page_Load(object sender, EventArgs e)
         {
 
---
+++ TP5_GRUPO_15/conexion.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient; 
+using System.Linq;
+using System.Web;
+
+namespace TP5_GRUPO_15
+{
+    public class conexion
+    {
+        private const string cadenaConexion = @"localhost\SQLEXPRESS;Initial Catalog = BDSucursales; Integrated Security = True;";
+
+        public int ejectutarConsultas(string consultaSql)
+        {
+
+            SqlConnection sqlConnection = new SqlConnection(cadenaConexion);
+
+            sqlConnection.Open();
+
+            SqlCommand sqlCommand = new SqlCommand(consultaSql, sqlConnection);
+
+            int filasAfectadas = sqlCommand.ExecuteNonQuery();
+
+            sqlConnection.Close();
+            return filasAfectadas;
+        }
+    }
+}
